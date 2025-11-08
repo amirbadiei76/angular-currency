4cd197cc287a0f887d21118ce90feecbd39d50ee
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-11-09 04:34:38 +1030

    Breadcrumb added

--- src/app/components/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/components/shared/breadcrumb/breadcrumb.component.ts
@@ -1,16 +1,24 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+
+export interface BreadcrumbItem {
+  title: string,
+  link?: string
+}
 
 @Component({
   selector: 'app-breadcrumb',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgFor, NgIf],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.css'
 })
 export class BreadcrumbComponent {
-  @Input() firstTitle!: string;
-  @Input() firstLink!: string;
-  @Input() secondTitle!: string;
-  @Input() secondLink!: string;
+  @Input() items: BreadcrumbItem[] = []
+  
+  constructor() {
+    console.log(this.items)
+  }
 }
--- src/app/pages/currency-item-details/currency-item-details.component.ts
+++ src/app/pages/currency-item-details/currency-item-details.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbComponent } from '../../components/shared/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-currency-item-details',
-  imports: [],
+  imports: [BreadcrumbComponent],
   templateUrl: './currency-item-details.component.html',
   styleUrl: './currency-item-details.component.css'
 })
 export class CurrencyItemDetailsComponent {
   title?: string = "";
 
-  constructor(private route: ActivatedRoute) {
+  breadCrumbItems = [
+    {
+      title: 'صفحه اصلی', link: '/'
+    },
+    {
+      title: this.title,
+    }
+  ]
 
+  constructor(private route: ActivatedRoute) {
+    console.log(this.breadCrumbItems)
   }
 
   ngOnInit() {

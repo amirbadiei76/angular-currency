7e697ae022d397a642bab9972c1ba3af28a32f7e
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-11-09 04:45:45 +1030

    Folder structure get better

--- src/app/pages/currency-item-details/currency-item-details.component.ts
+++ src/app/pages/currency-item-details/currency-item-details.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BreadcrumbComponent } from '../../components/shared/breadcrumb/breadcrumb.component';
+import { BreadcrumbComponent, BreadcrumbItem } from '../../components/shared/breadcrumb/breadcrumb.component';
+import { CurrenciesService } from '../../services/currencies.service';
+import { CurrencyItem } from '../../interface/Currencies';
+import { RequestArrayService } from '../../services/request-array.service';
 
 @Component({
   selector: 'app-currency-item-details',
@@ -9,24 +12,29 @@ import { BreadcrumbComponent } from '../../components/shared/breadcrumb/breadcru
   styleUrl: './currency-item-details.component.css'
 })
 export class CurrencyItemDetailsComponent {
-  title?: string = "";
+  title: string = "";
+  currencyItem?: CurrencyItem;
 
-  breadCrumbItems = [
-    {
-      title: 'صفحه اصلی', link: '/'
-    },
-    {
-      title: this.title,
-    }
-  ]
+  breadCrumbItems: BreadcrumbItem[] = []
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private requestArray: RequestArrayService) {
     console.log(this.breadCrumbItems)
   }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.title = params['title'];
+
+      this.currencyItem = this.requestArray.allItemsList.find((item) => item.slugText == this.title)!;
+
+      this.breadCrumbItems = [
+        {
+          title: 'صفحه اصلی', link: '/'
+        },
+        {
+          title: this.currencyItem.title,
+        }
+      ]
     })
   }
 }

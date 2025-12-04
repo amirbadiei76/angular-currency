d50b77fba8e6791723b938df325ee48241391154
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-04 22:56:17 +0330

    Related Item for currency details added

--- src/app/components/not-shared/currency-item-details/currency-overview/currency-overview.component.ts
+++ src/app/components/not-shared/currency-item-details/currency-overview/currency-overview.component.ts
@@ -14,9 +14,57 @@ export class CurrencyOverviewComponent {
   currentType = signal(0);
   currentList = signal(this.relatedItems?.slice(0, 5));
 
+  constructor () {
+    
+  }
+
+  ngOnInit () {
+    this.changeCurrentType(0)
+  }
+
   changeCurrentType (value: number) {
     this.currentType.set(value)
-    console.log(this.currentType())
+    switch (value) {
+      case 0:
+        this.setChange24hListAscending();
+        break;
+      case 1:
+        this.setChange24hListDescending();
+        break;
+    }
+  }
+
+  setChange24hListDescending () {
+    const descendingPriceList: CurrencyItem[] = [...this.relatedItems!!]
+    const mainDescList = descendingPriceList.sort((a: CurrencyItem, b: CurrencyItem) => {
+      const aValue = (a.lastPriceInfo.dt === 'high' ? '+' : '-') + a.lastPriceInfo.dp;
+      const bValue = (b.lastPriceInfo.dt === 'high' ? '+' : '-') + b.lastPriceInfo.dp;
+
+      const realAValue = aValue.startsWith('-') ? Number(aValue) : a.lastPriceInfo.dp;
+      const realBValue = bValue.startsWith('-') ? Number(bValue) : b.lastPriceInfo.dp;
+      
+      if (realAValue > realBValue) return 1
+      else return -1
+    })
+    console.log(mainDescList.slice(0, 5))
+    this.currentList.set(mainDescList?.slice(0, 5))
+  }
+
+  
+  setChange24hListAscending () {
+    const ascendingPriceList: CurrencyItem[] = [...this.relatedItems!!]
+    const mainAscList = ascendingPriceList.sort((a: CurrencyItem, b: CurrencyItem) => {
+      const aValue = (a.lastPriceInfo.dt === 'high' ? '+' : '-') + a.lastPriceInfo.dp;
+      const bValue = (b.lastPriceInfo.dt === 'high' ? '+' : '-') + b.lastPriceInfo.dp;
+
+      const realAValue = aValue.startsWith('-') ? Number(aValue) : a.lastPriceInfo.dp;
+      const realBValue = bValue.startsWith('-') ? Number(bValue) : b.lastPriceInfo.dp;
+      
+      if (realAValue > realBValue) return -1
+      else return 1
+    })
+    console.log(mainAscList.slice(0, 5))
+    this.currentList.set(mainAscList?.slice(0, 5))
   }
 
 }

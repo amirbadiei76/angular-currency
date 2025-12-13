70a167470db090b16f0c2ffa6a1dd6c458b48b9d
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-14 00:40:43 +0330

    Hamburger menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute, Route, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../app/components/shared/header/header.component';
 import { NotificationComponent } from './components/shared/notification/notification.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { MobileHambergerMenuComponent } from './components/shared/mobile-hamberger-menu/mobile-hamberger-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NotificationComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NotificationComponent, MobileHambergerMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

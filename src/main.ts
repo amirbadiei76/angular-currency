d1e60ed838cc80db2799394aa40e0599826f578d
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-21 14:00:16 +0330

    Service worker added

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));

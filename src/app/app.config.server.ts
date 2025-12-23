0ca30795c3136247d0b50f3b458327b6723da31b
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-23 19:24:36 +0330

    Render changed to Client

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,6 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
+// import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 // import { serverRoutes } from './app.routes.server';
 

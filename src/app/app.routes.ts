8696311d55f335f594700b74a08c25225395e794
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-23 13:30:42 +0330

    Path match added to home screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
-import { Routes } from '@angular/router';
+import { defaultUrlMatcher, Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('../app/pages/home/home.component').then((home) => home.HomeComponent), title: 'ارزیاب | مرجع قیمت بازارها' },
+    { path: '', pathMatch: 'full', loadComponent: () => import('../app/pages/home/home.component').then((home) => home.HomeComponent), title: 'ارزیاب | مرجع قیمت بازارها' },
     { path: 'gold-calculator', loadComponent: () => import('../app/pages/gold-calculator/gold-calculator.component').then((gold) => gold.GoldCalculatorComponent), title: 'ارزیاب | محاسبه‌گر طلا' },
     { path: 'converter', loadComponent: () => import('../app/pages/converter/converter.component').then((converter) => converter.ConverterComponent), title: 'ارزیاب | مبدل ارز' },
     { path: ':title', loadComponent: () => import('../app/pages/currency-item-details/currency-item-details.component').then((item) => item.CurrencyItemDetailsComponent) },

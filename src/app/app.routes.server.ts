fd0c27cebdd2adb19a07793263351b69c8d0dc27
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-23 18:43:01 +0330

    Render changed to prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,36 @@
 import { inject } from '@angular/core';
 import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
 import { RequestArrayService } from './services/request-array.service';
+import { ActivatedRoute } from '@angular/router';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
+  // {
+  //   path: ':title',
+  //   renderMode: RenderMode.Server
+  // },
+  // {
+  //   path: '**',
+  //   renderMode: RenderMode.Prerender,
 
+  // }
+  {
+    path: 'converter',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'gold-calculator',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
   },
   {
     path: ':title',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
     renderMode: RenderMode.Server
   }
 ];

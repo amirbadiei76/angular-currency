7bee288227b706202d257e1ff1727220cab2d858
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-23 15:54:18 +0330

    Render changed to prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,21 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { inject } from '@angular/core';
+import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
+import { RequestArrayService } from './services/request-array.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+
   },
   {
     path: ':title',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const requestService = inject(RequestArrayService)
+
+      const items = requestService.allItemsList.map((item) => ({title: item.slugText!}))
+      return items;
+    }
   }
 ];

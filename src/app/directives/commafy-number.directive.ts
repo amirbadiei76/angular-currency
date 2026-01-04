ea0f66410839a4a32bc79ff9c8fa2fcc72903033
Amirhossain Badiei <amir_badiei@yahoo.com>
2026-01-05 00:27:31 +0330

    Support farsi and arabic and english digits on input changed

--- src/app/directives/commafy-number.directive.ts
+++ src/app/directives/commafy-number.directive.ts
@@ -7,18 +7,50 @@ import { fromEvent } from 'rxjs';
 })
 export class CommafyNumberDirective {
   private el = inject(ElementRef<HTMLInputElement>);
+  private isComposing = false;
 
-  ngOnInit() {
-    fromEvent<InputEvent>(this.el.nativeElement, 'input')
-      .subscribe(() => this.format());
+  // ngOnInit() {
+  //   fromEvent<InputEvent>(this.el.nativeElement, 'input')
+  //     .subscribe(() => this.format());
+  // }
+
+  @HostListener('compositionstart')
+  onCompositionStart() {
+    this.isComposing = true;
   }
 
-  private commafyFromRight(value: string): string {
-    const reversed = value.split('').reverse().join('');
-    const grouped = reversed.match(/.{1,3}/g)?.join(',') ?? '';
-    return grouped.split('').reverse().join('');
+  @HostListener('compositionend')
+  onCompositionEnd() {
+    this.isComposing = false;
+    this.format(); // format فقط بعد از commit
   }
-  
+
+  @HostListener('input')
+  onInput() {
+    if (this.isComposing) return;
+    this.format();
+  }
+
+  private format() {
+    const input = this.el.nativeElement;
+    const cursor = input.selectionStart ?? 0;
+
+    const normalized = this.normalizeDigits(input.value);
+    const raw = normalized.replace(/\D+/g, '');
+
+    if (!raw) {
+      input.value = '';
+      return;
+    }
+
+    const formatted = raw.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    const diff = formatted.length - input.value.length;
+
+    input.value = formatted;
+    input.setSelectionRange(cursor + diff, cursor + diff);
+  }
+
   private normalizeDigits(value: string): string {
     const persian = '۰۱۲۳۴۵۶۷۸۹';
     const arabic  = '٠١٢٣٤٥٦٧٨٩';
@@ -34,24 +66,45 @@ export class CommafyNumberDirective {
     });
   }
 
-  format() {
-      const input = this.el.nativeElement;
-      const cursor = input.selectionStart ?? 0;
+  // private commafyFromRight(value: string): string {
+  //   const reversed = value.split('').reverse().join('');
+  //   const grouped = reversed.match(/.{1,3}/g)?.join(',') ?? '';
+  //   return grouped.split('').reverse().join('');
+  // }
+  
+  // private normalizeDigits(value: string): string {
+  //   const persian = '۰۱۲۳۴۵۶۷۸۹';
+  //   const arabic  = '٠١٢٣٤٥٦٧٨٩';
 
-      const normalized = this.normalizeDigits(input.value);
+  //   return value.replace(/[۰-۹٠-٩]/g, d => {
+  //     const p = persian.indexOf(d);
+  //     if (p !== -1) return p.toString();
+
+  //     const a = arabic.indexOf(d);
+  //     if (a !== -1) return a.toString();
+
+  //     return d;
+  //   });
+  // }
+
+  // format() {
+  //     const input = this.el.nativeElement;
+  //     const cursor = input.selectionStart ?? 0;
+
+  //     const normalized = this.normalizeDigits(input.value);
       
-      const raw = normalized.replace(/\D+/g, '');
+  //     const raw = normalized.replace(/\D+/g, '');
 
-      if (!raw) {
-        input.value = '';
-        return;
-      }
+  //     if (!raw) {
+  //       input.value = '';
+  //       return;
+  //     }
 
-      const formatted = raw.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  //     const formatted = raw.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-      const diff = formatted.length - input.value.length;
+  //     const diff = formatted.length - input.value.length;
 
-      input.value = formatted;
-      input.setSelectionRange(cursor + diff, cursor + diff);
-  }
+  //     input.value = formatted;
+  //     input.setSelectionRange(cursor + diff, cursor + diff);
+  // }
 }
